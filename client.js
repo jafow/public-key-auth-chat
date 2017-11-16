fd3ea091a6e68ae4a8c76abe354c424b26d2a82e
jafow <jared.a.fowler@gmail.com>
2017-11-16 11:13:24 -0800

    eslint

--- client.js
+++ client.js
@@ -5,7 +5,7 @@ const http = require('http')
 var publicKey = Buffer.allocUnsafe(salt.crypto_box_PUBLICKEYBYTES)
 
 // create a 32 byte length _nearly_ empty buffer that salt will make into a Secret Key
-var secretKey = Buffer.allocUnsafe(salt.crypto_box_SECRETKEYBYTES) 
+var secretKey = Buffer.allocUnsafe(salt.crypto_box_SECRETKEYBYTES)
 var message = 'doing the crypto is fun and cool'
 var mLen = message.length
 var cipher = Buffer.allocUnsafe(salt.crypto_box_SEALBYTES + mLen)
@@ -22,14 +22,14 @@ var req = http.request(opts, (res) => {
     salt.crypto_box_seal(cipher, Buffer.from(message, 'utf8'), rk)
 
     // post the message back to them
-    var req1 = http.request({hostname: 'localhost', port:3322, method: 'POST', path: '/msg'}, (_res) => {
+    var req1 = http.request({hostname: 'localhost', port: 3322, method: 'POST', path: '/msg'}, (_res) => {
       _res.on('end', () => { console.log('ended') })
-      _res.on('data', (data) => { console.log('got data: ', data.toString())})
+      _res.on('data', (data) => { console.log('got data: ', data.toString()) })
     })
     req1.write(cipher)
     req1.end()
   })
 })
 
-req.on('err', (e) => { console.error('error: ', e)})
+req.on('err', (e) => { console.error('error: ', e) })
 req.end()
--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 const salt = require('sodium-native')
 const http = require('http')
-const querystring = require('querystring')
 
 // create a 32 byte length _nearly_ empty buffer that salt will make into a Public Key
 var publicKey = Buffer.allocUnsafe(salt.crypto_box_PUBLICKEYBYTES)
 
 // create a 32 byte length _nearly_ empty buffer that salt will make into a Secret Key
-var secretKey = Buffer.allocUnsafe(salt.crypto_box_SECRETKEYBYTES) 
+var secretKey = Buffer.allocUnsafe(salt.crypto_box_SECRETKEYBYTES)
 salt.crypto_box_keypair(publicKey, secretKey)
 
 const server = http.createServer(function (req, res) {
-  if (req.url === '/pk' && req.method == 'GET') {
+  if (req.url === '/pk' && req.method === 'GET') {
     res.write(publicKey)
     res.end()
-  } else if (req.url == '/msg' && req.method === 'POST') {
+  } else if (req.url === '/msg' && req.method === 'POST') {
     req.on('data', (data) => {
       let cipher = data
       let cipherLen = cipher.length
@@ -22,13 +21,13 @@ const server = http.createServer(function (req, res) {
 
       salt.crypto_box_seal_open(
         decryptedMsgBytes,
-        Buffer.from(cipher), 
+        Buffer.from(cipher),
         publicKey,
         secretKey
       )
 
-      let dec = decryptedMsgBytes.toString() 
-      console.log(`decrypted: ${dec}`);
+      let dec = decryptedMsgBytes.toString()
+      console.log(`decrypted: ${dec}`)
       res.end('ok\n')
     })
   } else {
