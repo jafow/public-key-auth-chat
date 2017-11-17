2c271df2fb20494c84dd76d4066c3b9e6be09c14
jafow <jared.a.fowler@gmail.com>
2017-11-16 21:31:31 -0800

    Makes PORT & HOST into variables:
    We are going to change these up a few times

--- client.js
+++ client.js
@@ -1,28 +1,28 @@
-const salt = require('sodium-native')
+const sodium = require('sodium-native')
 const http = require('http')
 
-// create a 32 byte length _nearly_ empty buffer that salt will make into a Public Key
-var publicKey = Buffer.allocUnsafe(salt.crypto_box_PUBLICKEYBYTES)
+// create a 32 byte length _nearly_ empty buffer that sodium will make into a Public Key
+var publicKey = Buffer.allocUnsafe(sodium.crypto_box_PUBLICKEYBYTES)
 
-// create a 32 byte length _nearly_ empty buffer that salt will make into a Secret Key
-var secretKey = Buffer.allocUnsafe(salt.crypto_box_SECRETKEYBYTES)
+// create a 32 byte length _nearly_ empty buffer that sodium will make into a Secret Key
+var secretKey = Buffer.allocUnsafe(sodium.crypto_box_SECRETKEYBYTES)
 var message = 'doing the crypto is fun and cool'
 var mLen = message.length
-var cipher = Buffer.allocUnsafe(salt.crypto_box_SEALBYTES + mLen)
+var cipher = Buffer.allocUnsafe(sodium.crypto_box_SEALBYTES + mLen)
 
 // make the keys
-salt.crypto_box_keypair(publicKey, secretKey)
+sodium.crypto_box_keypair(publicKey, secretKey)
 
 // get the recipients PK:
-var opts = {hostname: 'localhost', port: 3322, method: 'GET', path: '/pk'}
+var opts = {hostname: HOST, port: PORT, method: 'GET', path: '/pk'}
 var req = http.request(opts, (res) => {
   res.on('data', (d) => {
     let rk = d
     // encrypt the ciphertext with recipient's pk that we got from their server
-    salt.crypto_box_seal(cipher, Buffer.from(message, 'utf8'), rk)
+    sodium.crypto_box_seal(cipher, Buffer.from(message, 'utf8'), rk)
 
     // post the message back to them
-    var req1 = http.request({hostname: 'localhost', port: 3322, method: 'POST', path: '/msg'}, (_res) => {
+    var req1 = http.request({hostname: HOST, port: PORT, method: 'POST', path: '/msg'}, (_res) => {
       _res.on('end', () => { console.log('ended') })
       _res.on('data', (data) => { console.log('got data: ', data.toString()) })
     })
